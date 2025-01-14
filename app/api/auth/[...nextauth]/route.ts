44e2383d9dad07664a937efe2301d588ca798d5c
Michael-Elrod <elrod.michael95@gmail.com>
2025-01-14 18:45:39 -0500

    .

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,6 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/app/lib/auth";
 
-// Let NextAuth handle the response formatting
 const handler = NextAuth(authOptions);
 
-export const GET = handler;
-export const POST = handler;
+export { handler as GET, handler as POST }
