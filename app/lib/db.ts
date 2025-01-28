b441788fa3b4add73bf928d35ab36fb02f1c1170
Michael-Elrod <elrod.michael95@gmail.com>
2025-01-27 21:29:27 -0500

    db logs

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -20,6 +20,22 @@ const pool = global.mysqlPool || mysql.createPool({
   keepAliveInitialDelay: 0
 });
 
+pool.on('acquire', function (connection) {
+  console.log('Connection %d acquired', connection.threadId);
+});
+
+pool.on('connection', function (connection) {
+  console.log('New connection made, id: %d', connection.threadId);
+});
+
+pool.on('enqueue', function () {
+  console.log('Waiting for available connection slot');
+});
+
+pool.on('release', function (connection) {
+  console.log('Connection %d released', connection.threadId);
+});
+
 pool.getConnection()
   .then(connection => {
     connection.release();
